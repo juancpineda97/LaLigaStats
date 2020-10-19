5fd3912840a8262d6941ec289f01a8201fa5c8b2
juancpineda97 <juancpineda2@gmail.com>
2020-10-19 20:58:23 +0200

    Añade al método verEquipo() la salida propia en el caso de que el equipo esté vacío refs #4

--- src/equipo.js
+++ src/equipo.js
@@ -16,14 +16,22 @@ class Equipo{
     
     /**
      * Método que muestra todos los jugadores de un equipo.
-     * @returns {String} Datos de los jugadoresd el equipo dado.
+     * @returns {String} Datos de los jugadores del equipo dado.
      */
     verEquipo(){
-        var jugadores = "Lista de jugadores del " + this.nombre + ":";
+
+        var jugadores;
+
+        if(Object.keys(this.listaJugadores).length === 0){
+            jugadores = "Equipo sin jugadores actualmente.";
+        }
+        else{
+            jugadores = "Lista de jugadores del " + this.nombre + ":";
         
-        this.listaJugadores.forEach(function(elemento) {
-            jugadores += "\n\n" + elemento.verJugador();
-        })
+            this.listaJugadores.forEach(function(elemento) {
+                jugadores += "\n\n" + elemento.verJugador();
+            })
+        }
 
         return jugadores;
     }

054d86d77a3c4933eb6c6e7c1f4494bdcf5f076f
juancpineda97 <juancpineda2@gmail.com>
2020-10-29 21:12:45 +0100

    Añade comentarios Fixes #35

--- src/asserts.js
+++ src/asserts.js
@@ -4,12 +4,15 @@ class Asercion {
     
     expect(actual) {
         return {
+
+            //Comprueba si el valor actual es igual al esperado
             toEqual(expected) {
                 if (actual !== expected) {
                     throw new Error(`${actual} no es igual al valor esperado: ${expected}`);
                 }
             },
 
+            //Comprueba si el valor actual es del tipo esperado
             toBeType(expected){
                 var tipo = typeof actual;
                 if (tipo != expected) {
@@ -17,18 +20,21 @@ class Asercion {
                 }
             },
 
+            //Comprueba si el valor actual incluye el valor esperado
             toInclude(expected){
                 if (actual.includes(expected) !== true){
                     throw new Error(`[${actual}] \nno incluye el siguiente valor: ${expected}`);
                 }
             },
-          
+
+            //Comprueba si el valor actual es menor que el esperado
             toBeLowerThan(expected) {
                 if (actual < expected) {
                     throw new Error(`${actual} es menor que el valor esperado: ${expected}`);
                 }
             },
 
+            //Comprueba si el valor actual es menor que el esperado
             toBeGreaterThan(expected) {
                 if (actual > expected) {
                     throw new Error(`${actual} es mayor que el valor esperado: ${expected}`);

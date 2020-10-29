b67b4804b0a04e2d28aec3a1c229deb63a4fce00
juancpineda97 <juancpineda2@gmail.com>
2020-10-29 20:18:57 +0100

    Implementa la clase de las aserciones Fixes #35

---
+++ src/asserts.js
@@ -0,0 +1,28 @@
+//Biblioteca de aserciones
+
+class Asercion {
+    
+    expect(actual) {
+        return {
+            toEqual(expected) {
+                if (actual !== expected) {
+                    throw new Error(`${actual} no es igual al valor esperado: ${expected}`);
+                }
+            },
+          
+            toBeLowerThan(expected) {
+                if (actual < expected) {
+                    throw new Error(`${actual} es menor que el valor esperado: ${expected}`);
+                }
+            },
+
+            toBeGreaterThan(expected) {
+                if (actual > expected) {
+                    throw new Error(`${actual} es mayor que el valor esperado: ${expected}`);
+                }
+            },
+        }
+    }
+}
+
+module.exports = Asercion;

1eb15cc3b93847a403df9782f0a40bc1985928fe
juancpineda97 <juancpineda2@gmail.com>
2020-12-20 23:27:16 +0100

    Implementa 1º aproximación: se devuelven los jugadores del equipo refs #78 refs #4

--- api/getEquipo.js
+++ api/getEquipo.js
@@ -1,6 +1,14 @@
 const data = require("../data/equipos.json");
 var archivo = JSON.parse(JSON.stringify(data));
+var keys = Object.keys(archivo);
 
 module.exports = (req, res) => {
-
+    var equipo = req.query['equipo'];
+    if (keys.includes(equipo)){
+        var equipo_completo = archivo[equipo];
+        res.status(200).send(equipo_completo);
+    }
+    else{
+        res.status(404).send("Equipo no encontrado");
+    }
 }

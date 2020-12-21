e26df857417accc84943f98a23e62f6fb9cde3f7
juancpineda97 <juancpineda2@gmail.com>
2020-12-22 00:01:56 +0100

    Implementa getRanking con valores de equipos desordenados refs #81 refs #80

--- functions/getRanking.js
+++ functions/getRanking.js
@@ -1,1 +1,22 @@
-exports.handler = async event => {}
+const data = require("../data/equipos.json");
+var archivo = JSON.parse(JSON.stringify(data));
+var keys = Object.keys(archivo);
+
+exports.handler = async event => {
+
+    var valores_equipos = {};
+    
+    keys.forEach(equipo => {
+        var valor_acumulado = 0.0;
+        archivo[equipo].forEach(jugador => {
+            valor_acumulado = (parseFloat(valor_acumulado) + parseFloat(jugador['valor'])).toFixed(1);
+        });
+        valores_equipos[equipo] = valor_acumulado;
+    });
+
+    return {
+        statusCode:200,
+        body:JSON.stringify(valores_equipos)
+    }
+
+}

15963ac91e2f5f90c7972388c60e1e42050bbf4d
juancpineda97 <juancpineda2@gmail.com>
2020-12-24 02:31:25 +0100

    Finaliza implementación función getRankingValorEquipos refs #83 refs #80

--- src/utils.js
+++ src/utils.js
@@ -69,6 +69,53 @@ function getJugadoresEquipo(JSON, equipo){
  * @param {boolean} JSON - True si se quiere la salida en formato de JSON, false si se quiere en formato de string.
  */
 function getRankingValorEquipos(JSON){
+    
+    if(keys.length == 0){
+        return "";
+    }
+    else{
+        var valores_equipos = {};
+        
+        keys.forEach(equipo => {
+            var valor_acumulado = 0.0;
+            archivo[equipo].forEach(jugador => {
+                valor_acumulado = (parseFloat(valor_acumulado) + parseFloat(jugador['valor'])).toFixed(1);
+            });
+            valores_equipos[equipo] = valor_acumulado;
+        });
+
+        var valores_equipos_array= Object.entries(valores_equipos);
+        valores_equipos_array.sort((a,b) => b[1] -a[1]);
+
+        var valores_equipos_ordenados = {};
+
+        valores_equipos_array.forEach(equipo => {
+            if ((equipo[1] % 1) == 0){
+                valores_equipos_ordenados[equipo[0]] = Math.trunc(equipo[1]);
+            }
+            else{
+                valores_equipos_ordenados[equipo[0]] = equipo[1];
+            }     
+        });
+
+        if (JSON){
+            return valores_equipos_ordenados;
+        }
+        else{
+            var mensaje = "Ranking de equipos más valiosos de LaLiga:" + "\n\n";
+            var n = 1;
+            for (const equipo_valor in valores_equipos_ordenados) {
+                mensaje = mensaje + n.toString() + " -> " + equipo_valor + ": " +
+                valores_equipos_ordenados[equipo_valor].toString() + " M(€)";
+                if (n < keys.length){
+                    mensaje = mensaje + "\n";
+                }
+                n++;
+            }
+
+            return mensaje;
+        }
+    }
 
 }
 

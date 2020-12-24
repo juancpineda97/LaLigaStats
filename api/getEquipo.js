6a038bdcef1c7730e8b7dca8f877b905a900486c
juancpineda97 <juancpineda2@gmail.com>
2020-12-24 03:43:18 +0100

    Añade función de utils.js a getEquipo.js refs #85 refs #83

--- api/getEquipo.js
+++ api/getEquipo.js
@@ -1,6 +1,4 @@
-const data = require("../data/equipos.json");
-var archivo = JSON.parse(JSON.stringify(data));
-var keys = Object.keys(archivo);
+const utils = require("../src/utils.js");
 
 module.exports = (req, res) => {
     var equipo = req.query['equipo'];
@@ -11,24 +9,10 @@ module.exports = (req, res) => {
         res.status(400).send("Por favor, indique un nombre de equipo con al menos 3 letras");
     }
     else{
-        var equipos_a_mostrar = [];
-        var re = new RegExp(equipo, "i");
-    
-        for (var i in keys){
-            if (keys[i].toString().match(re)){
-                equipos_a_mostrar.push(keys[i]);
-            }
-        }
+        var salida = utils.getJugadoresEquipo(true, equipo);
 
-        if (equipos_a_mostrar.length > 0){
-            var mensaje = {};
-            for (let j = 0; j < equipos_a_mostrar.length; j++) {
-                //mensaje = mensaje + JSON.stringify(archivo[equipos_a_mostrar[j]]);
-                var equipo_temp = [];
-                equipo_temp = archivo[equipos_a_mostrar[j]];
-                mensaje[equipos_a_mostrar[j]] = equipo_temp;
-            }
-            res.status(200).send(mensaje);
+        if (salida != ""){
+            res.status(200).send(salida);
         }
         else{
             res.status(404).send("Equipo no encontrado");

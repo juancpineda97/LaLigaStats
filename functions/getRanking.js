bdb2c4eb37935afa0678d827e76ee055e98feb1e
juancpineda97 <juancpineda2@gmail.com>
2020-12-24 03:51:33 +0100

    Añade función de utils.js a getRanking.js closes #85 closes #83

--- functions/getRanking.js
+++ functions/getRanking.js
@@ -1,44 +1,18 @@
-const data = require("../data/equipos.json");
-var archivo = JSON.parse(JSON.stringify(data));
-var keys = Object.keys(archivo);
+const utils = require("../src/utils.js");
 
 exports.handler = async event => {
-
-    if(keys.length == 0){
+    var ranking = utils.getRankingValorEquipos(true);
+    
+    if(ranking != ""){
+        return {
+            statusCode:200,
+            body:JSON.stringify(ranking)
+        }
+    }
+    else{
         return {
             statusCode:500,
             body:"INTERNAL ERROR: No hay ningun equipo disponible para mostrar"
-        }
-    }
-
-    else{
-        var valores_equipos = {};
-        
-        keys.forEach(equipo => {
-            var valor_acumulado = 0.0;
-            archivo[equipo].forEach(jugador => {
-                valor_acumulado = (parseFloat(valor_acumulado) + parseFloat(jugador['valor'])).toFixed(1);
-            });
-            valores_equipos[equipo] = valor_acumulado;
-        });
-
-        var valores_equipos_array= Object.entries(valores_equipos);
-        valores_equipos_array.sort((a,b) => b[1] -a[1]);
-
-        var valores_equipos_ordenados = {};
-
-        valores_equipos_array.forEach(equipo => {
-            if ((equipo[1] % 1) == 0){
-                valores_equipos_ordenados[equipo[0]] = Math.trunc(equipo[1]);
-            }
-            else{
-                valores_equipos_ordenados[equipo[0]] = equipo[1];
-            }     
-        });
-
-        return {
-            statusCode:200,
-            body:JSON.stringify(valores_equipos_ordenados)
         }
     }
 
